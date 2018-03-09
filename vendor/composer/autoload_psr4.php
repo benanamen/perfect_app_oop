a78fd08f3fbeab3883a038eeda0d725d157a7ef3
KR <ducatikevin@yahoo.com>
2018-03-09 13:23:46 -0800

    Updated Composer Files

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Psr\\Log\\' => array($vendorDir . '/psr/log/Psr/Log'),
     'PerfectApp\\' => array($vendorDir . '/krubio/perfect_app_oop/src'),
+    'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
 );
