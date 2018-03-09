17ccb9d2d7c612c8324374dfbfcc301a9411a020
KR <ducatikevin@yahoo.com>
2018-03-09 13:18:39 -0800

    Created Class for showing Debug Data ($_GET, $_POST, $_SESSION, $_REQUEST, $_COOKIE)

---
+++ vendor/krubio/perfect_app_oop/src/Debug/ShowDebugData.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace PerfectApp\Debug;
+
+
+/**
+ * Class ShowDebugData
+ * @package PerfectApp\Debug
+ */
+class ShowDebugData
+{
+    /**
+     *
+     */
+    public static function showDebugData()
+    {
+        echo '<div class="error_custom"><H1>DEBUGGING IS ON !!!</H1></div>';
+
+        $var = null;
+
+        if (SHOW_COOKIE_DATA)
+        {
+            $var['COOKIE'] = $_COOKIE;
+        }
+
+        if (SHOW_SESSION_DATA && isset($_SESSION))
+        {
+            $var['SESSION'] = $_SESSION;
+        }
+
+        if (SHOW_POST_DATA)
+        {
+            $var['POST'] = $_POST;
+        }
+
+        if (SHOW_GET_DATA)
+        {
+            $var['GET'] = $_GET;
+        }
+
+        if (SHOW_REQUEST_DATA)
+        {
+            $var['REQUEST'] = $_REQUEST;
+        }
+
+        if ($var)
+        {
+            $varDumper = new HTMLVarDumper();
+
+            foreach ($var as $title => $data)
+            {
+                $varDumper->dump($title, $data);
+            }
+        }
+    }
+}
