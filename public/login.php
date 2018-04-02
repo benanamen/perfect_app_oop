5df77cd7a2eb57db6e48c5f6428f070c75f9fd63
KR <ducatikevin@yahoo.com>
2018-04-02 10:14:39 -0700

    Minor changes to login.php

--- public/login.php
+++ public/login.php
@@ -1,8 +1,9 @@
 <?php
-ob_start();// Only needed when Debugging is turned on
 /**
- * Last Modified <!--%TimeStamp%-->12/1/2017 8:37 PM<!---->
+ * Last Modified <!--%TimeStamp%-->03/09/2018 9:21 PM<!---->
  */
+
+ob_start();// Only needed when Debugging is turned on
 
 use PerfectApp\Logging\SQLLoginAttemptsLog;
 
@@ -17,7 +18,7 @@ $error = [];
 $show_error = false;
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
-    {
+{
     $_POST = array_map('trim', $_POST);
 
     //------------------------------------------------------------------------------------
@@ -25,40 +26,38 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     //------------------------------------------------------------------------------------
 
     if (empty($_POST['username']))
-        {
+    {
         $error['username'] = 'Username Required.';
-        }
+    }
     if (empty($_POST['password']))
-        {
+    {
         $error['password'] = 'Password Required.';
-        }
+    }
 
     //------------------------------------------------------------------------------------
     // Display Errors
     //------------------------------------------------------------------------------------
 
     if ($error)
-        {
+    {
         $show_error = true;
-        }
+    }
     else
-        {
+    {
         $login_attempt = new SQLLoginAttemptsLog($pdo);
 
-        $sql  = "SELECT username, password, first_name, last_name, is_active FROM users WHERE username = ?";
+        $sql = "SELECT password, first_name, last_name, is_active FROM users WHERE username = ?";
         $stmt = $pdo->prepare($sql);
-        $stmt->execute([
-            $_POST['username']
-        ]);
+        $stmt->execute([$_POST['username']]);
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        // Username didnt match, Redirect.
+        // Username didn't match, Redirect.
         if (!$row)
-            {
+        {
             $login_attempt->logFailedAttempt($_POST['username']);
             header("Location: {$_SERVER['SCRIPT_NAME']}?failed_login");
             die;
-            }
+        }
 
         //--------------------------------------------------------------------------------
         // Compare the password to the expected hash.
@@ -66,51 +65,46 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 
         // Password is good
         if (password_verify($_POST['password'], $row['password']))
-            {
+        {
             // Person status is inactive
-             if (!$row['is_active'])
-                {
+            if (!$row['is_active'])
+            {
                 header("Location: ./login.php?inactive");
                 die;
-                }
+            }
 
             //----------------------------------------------------------------------------
-            // Log sucessful login attempt & Update Last Login Datetime
+            // Log successful login attempt & Update Last Login Datetime
             //----------------------------------------------------------------------------
 
-            $sql  = "UPDATE users SET last_login = NOW() WHERE username = ?;";
+            $login_attempt->logSuccessfulAttempt($_POST['username']);
+
+            $sql = "UPDATE users SET last_login = NOW() WHERE username = ?;";
             $stmt = $pdo->prepare($sql);
-            $stmt->execute([
-                $row['username']
-            ]);
+            $stmt->execute([$_POST['username']]);
 
             //----------------------------------------------------------------------------
             // Set Session Variables
             //----------------------------------------------------------------------------
 
             session_start();
-
-            // Destroy old session id and creating a new one
-            // Stops Session Fixation
             session_regenerate_id(true);
 
             $_SESSION['login'] = true;
             $_SESSION['first_name'] = $row['first_name'];
-            $_SESSION['last_name']  = $row['last_name'];
-
-            $login_attempt->logSuccessfulAttempt($_POST['username']);
+            $_SESSION['last_name'] = $row['last_name'];
 
             header("Location: ./index.php");
             die;
-            } // End if password_verify
+        } // End if password_verify
         else
-            {
+        {
             $login_attempt->logFailedAttempt($_POST['username']);
             header("Location: {$_SERVER['SCRIPT_NAME']}?failed_login");
             die;
-            }
-        } // End Else
-    } // End if Post
+        }
+    } // End Else
+} // End if Post
 
 // ---------------------------------------------------------------------------------------
 // Login Form
@@ -118,7 +112,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 
 include('./includes/header.php');
 logo(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_ALT);
+
 use PerfectApp\Utilities\DisplayActionMessage;
+
 echo DisplayActionMessage::actionMessage();
 
 // ---------------------------------------------------------------------------------------
@@ -126,9 +122,9 @@ echo DisplayActionMessage::actionMessage();
 // ---------------------------------------------------------------------------------------
 
 if ($show_error)
-    {
+{
     show_form_errors($error);
-    }
+}
 
 // ---------------------------------------------------------------------------------------
 // Display Form & Footer
