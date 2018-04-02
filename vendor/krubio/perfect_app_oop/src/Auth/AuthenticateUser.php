b5100ea49b0709635164a246514824a3432a6b4b
KR <ducatikevin@yahoo.com>
2018-04-02 10:18:21 -0700

    Started new user login OOP code.

--- vendor/krubio/perfect_app_oop/src/Auth/AuthenticateUser.php
+++ vendor/krubio/perfect_app_oop/src/Auth/AuthenticateUser.php
@@ -8,8 +8,45 @@
 
 namespace PerfectApp\Auth;
 
+use PDO;
 
+/**
+ * Class AuthenticateUser
+ * @package PerfectApp\Auth
+ */
 class AuthenticateUser
 {
+    /**
+     * @var PDO the connection to the underlying database
+     */
+    protected $pdo;
 
+    /**
+     * AuthenticateUser constructor.
+     * @param PDO $pdo
+     */
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    /**
+     * Checks whether a username/password combination is valid
+     *
+     * @param string $username the username to be checked
+     * @param string $password the corresponding password
+     *
+     * @return boolean the result of the check
+     */
+    public function check($username, $password)
+    {
+        $passwordHashStmt = $this->pdo->prepare('
+    SELECT password
+    FROM users
+    WHERE username = ?
+');
+        $passwordHashStmt->execute([$username]);
+        $passwordHash = $passwordHashStmt->fetchColumn();
+        return $passwordHash && password_verify($password, $passwordHash);
+    }
 }
