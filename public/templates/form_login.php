4e479f12baacc78a371ed5fc60411febe8c6407f
KR <ducatikevin@yahoo.com>
2018-04-20 12:33:27 -0700

    Latest Login form

--- public/templates/form_login.php
+++ public/templates/form_login.php
@@ -1,50 +1,52 @@
-<div class="row justify-content-center">
-    <div class="col-6">
-
 <form method="post" autocomplete="off">
-    <div class="form-group row">
-        <label class="col-3 col-form-label" for="username">Username <span class="text-danger">*</span></label>
-        <div class="col-6">
-            <input id="username" name="username" placeholder="Username" class="form-control <?= !empty($error['username']) ? 'is-invalid' : '' ?>" aria-describedby="usernameHelpBlock" type="text" value="<?= !empty($_POST['username']) ? htmlspecialchars($_POST['username']) : '' ?>" autofocus>
 
-            <?php if (!empty($error['username'])): ?>
-            <span id="usernameHelpBlock" class="form-text <?= !empty($error['username']) ? 'invalid-feedback' : 'text-muted' ?>"><?= $error['username'] ?></span>
-            <?php endif;?>
+    <div class="d-flex p-2 d-flex justify-content-center">
+        <div class="form-group row">
+            <label for="username" class="col-sm-2 col-md-6 col-form-label">Username <span style="color: #FF0000;">*</span></label>
+            <div class="col-sm-12 col-md-12">
+                <input id="username"
+                       type="text"
+                       name="username"
+                       class="form-control <?= !empty($error['username']) ? 'is-invalid' : '' ?>"
+                       placeholder="Username"
+                       value="<?= !empty($_POST['username']) ? htmlspecialchars($_POST['username'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') : '';?>">
 
-        </div>
-    </div>
+                <?php if (!empty($error['username'])): ?>
+                    <span id="passwordHelpBlock"
+                          class="form-text <?= !empty($error['username']) ? 'invalid-feedback' : '' ?>"><?= $error['username'] ?></span>
+                <?php endif; ?>
 
-    <div class="form-group row">
-        <label for="password" class="col-3 col-form-label">Password <span class="text-danger">*</span></label>
-        <div class="col-6">
-            <input id="password" name="password" placeholder="Password" class="form-control <?= !empty($error['password']) ? 'is-invalid' : '' ?>" aria-describedby="passwordHelpBlock" type="text" value="<?= !empty($_POST['password']) ? htmlspecialchars($_POST['password']) : '' ?>" >
-
-            <?php if (!empty($error['password'])): ?>
-            <span id="passwordHelpBlock" class="form-text <?= !empty($error['password']) ? 'invalid-feedback' : 'text-muted' ?>"><?= $error['password'] ?></span>
-            <?php endif;?>
-
-        </div>
-    </div>
-
-<!--    <div class="form-group">
-        <div class="form-check">
-            <input class="form-check-input is-invalid" type="checkbox" value="" id="invalidCheck3" required>
-            <label class="form-check-label" for="invalidCheck3">
-                Agree to terms and conditions
-            </label>
-            <div class="invalid-feedback">
-                You must agree before submitting.
             </div>
         </div>
-    </div>-->
+    </div>
 
+    <div class="d-flex p-2 d-flex justify-content-center">
+        <div class="form-group row">
+            <label for="password" class="col-sm-2 col-md-6 col-form-label">Password <span style="color: #FF0000;">*</span></label>
+            <div class="col-sm-12 col-md-12">
+                <input id="password"
+                       type="password"
+                       name="password"
+                       class="form-control <?= !empty($error['password']) ? 'is-invalid' : '' ?>"
+                       placeholder="Password"
+                       value="<?= !empty($_POST['password']) ? htmlspecialchars($_POST['password'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') : '';?>">
 
-    <div class="form-group row">
-        <div class="offset-3 col-1">
-            <button name="submit" type="submit" class="btn btn-primary">Sign In</button>
+                <?php if (!empty($error['password'])): ?>
+                    <span id="passwordHelpBlock"
+                          class="form-text <?= !empty($error['password']) ? 'invalid-feedback' : '' ?>"><?= $error['password'] ?></span>
+                <?php endif; ?>
+
+            </div>
         </div>
     </div>
-</form>
 
+    <div class="d-flex p-2 d-flex justify-content-center">
+        <div class="form-group row">
+            <div class="col-sm-12 col-md-12">
+                <button name="submit" type="submit" class="btn btn-primary">Sign In</button>&nbsp;
+                <a href="./forgot.php">Forgot Password</a>
+            </div>
+        </div>
     </div>
-</div>
+
+</form>
