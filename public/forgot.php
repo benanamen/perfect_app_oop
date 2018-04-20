26624b8d0455bbe8397bd141d9a00e082d09aac0
KR <ducatikevin@yahoo.com>
2018-04-20 13:05:19 -0700

    Moved Forgot Password Form to Template

--- public/forgot.php
+++ public/forgot.php
@@ -116,7 +116,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 // Forgot Password Form
 //----------------------------------------------------------------------------------------
 
-include('./includes/header.php');
+include './includes/header.php';
 logo(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_ALT);
 
 // ---------------------------------------------------------------------------------------
@@ -131,43 +131,6 @@ if ($show_error)
 // ---------------------------------------------------------------------------------------
 // Display Form
 // ---------------------------------------------------------------------------------------
-?>
 
-    <div class="row">
-        <div class="col-md-12">
-            <div class="col-md-6 offset-md-3">
-
-                <!-- form card reset password -->
-                <div class="card card-outline-secondary">
-                    <div class="card-header bg-primary text-white">
-                        <h3 class="mb-0">Password Reset</h3>
-                    </div>
-                    <div class="card-body">
-                        <form role="form" method="post" autocomplete="off">
-                            <div class="form-group">
-                                <label for="inputResetPasswordEmail">Email</label>
-                                <input id="inputResetPasswordEmail"
-                                       name="email"
-                                       type="email"
-                                       class="form-control <?= !empty($error['email']) ? 'is-invalid' : '' ?>">
-
-                                <?php if (!empty($error['email'])): ?>
-                                    <span class="form-text <?= !empty($error['email']) ? 'invalid-feedback' : '' ?>"><?= $error['email'] ?></span>
-                                <?php endif; ?>
-
-                                <span id="helpResetPasswordEmail" class="form-text small text-muted">Password reset instructions will be sent to this email address.</span>
-                            </div>
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-primary btn-md float-right">Reset</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-
-
-            </div>
-        </div>
-    </div>
-
-<?php
-include('./includes/footer.php');
+include './templates/form_forgot_password.php';
+include './includes/footer.php';
---
+++ public/templates/form_forgot_password.php
@@ -0,0 +1,41 @@
+<?php
+if (!defined('SECURE_PAGE'))
+{
+    die('<h1>Direct File Access Prohibited</h1>');
+}
+?>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="col-md-6 offset-md-3">
+
+            <!-- form card reset password -->
+            <div class="card card-outline-secondary">
+                <div class="card-header bg-primary text-white">
+                    <h3 class="mb-0">Password Reset</h3>
+                </div>
+                <div class="card-body">
+                    <form role="form" method="post" autocomplete="off">
+                        <div class="form-group">
+                            <label for="inputResetPasswordEmail">Email</label>
+                            <input id="inputResetPasswordEmail"
+                                   name="email"
+                                   type="email"
+                                   class="form-control <?= !empty($error['email']) ? 'is-invalid' : '' ?>">
+
+                            <?php if (!empty($error['email'])): ?>
+                                <span class="form-text <?= !empty($error['email']) ? 'invalid-feedback' : '' ?>"><?= $error['email'] ?></span>
+                            <?php endif; ?>
+
+                            <span id="helpResetPasswordEmail" class="form-text small text-muted">Password reset instructions will be sent to this email address.</span>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary btn-md float-right">Reset</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
