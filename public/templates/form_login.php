e2b5cddd9f812796eabb11b49ff284c6081477d4
KR <ducatikevin@yahoo.com>
2018-04-21 10:37:42 -0700

    Updated Login Form

--- public/templates/form_login.php
+++ public/templates/form_login.php
@@ -4,56 +4,58 @@ if (!defined('SECURE_PAGE'))
     die('<h1>Direct File Access Prohibited</h1>');
 }
 ?>
+<div class="row">
+    <div class="col-md-12">
+        <div class="col-md-6 offset-md-3">
 
-<form method="post" autocomplete="off">
+            <!-- form card -->
+            <div class="card card-outline-secondary">
+                <div class="card-header bg-primary text-white">
+                    <h3 class="mb-0">Login</h3>
+                </div>
+                <div class="card-body">
+                    <form role="form" method="post" autocomplete="off"
+                    ">
 
-    <div class="d-flex p-2 d-flex justify-content-center">
-        <div class="form-group row">
-            <label for="username" class="col-sm-2 col-md-6 col-form-label">Username <span style="color: #FF0000;">*</span></label>
-            <div class="col-sm-12 col-md-12">
-                <input id="username"
-                       type="text"
-                       name="username"
-                       class="form-control <?= !empty($error['username']) ? 'is-invalid' : '' ?>"
-                       placeholder="Username"
-                       value="<?= !empty($_POST['username']) ? htmlspecialchars($_POST['username'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') : '';?>">
+                    <div class="form-group">
+                        <label for="username">Username <span style="color: #FF0000;">*</span></label>
+                        <input id="username"
+                               name="username"
+                               type="text"
+                               class="form-control <?= !empty($error['username']) ? 'is-invalid' : '' ?>"
+                               placeholder="Username"
+                               value="<?= !empty($_POST['username']) ? htmlspecialchars($_POST['username'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') : ''; ?>"
+                        >
 
-                <?php if (!empty($error['username'])): ?>
-                    <span id="passwordHelpBlock"
-                          class="form-text <?= !empty($error['username']) ? 'invalid-feedback' : '' ?>"><?= $error['username'] ?></span>
-                <?php endif; ?>
+                        <?php if (!empty($error['username'])): ?>
+                            <span class="form-text <?= !empty($error['username']) ? 'invalid-feedback' : '' ?>"><?= $error['username'] ?></span>
+                        <?php endif; ?>
+                    </div>
 
+                    <div class="form-group">
+                        <label for="password">Password <span style="color: #FF0000;">*</span></label>
+                        <input id="password"
+                               name="password"
+                               type="text"
+                               class="form-control <?= !empty($error['password']) ? 'is-invalid' : '' ?>"
+                               placeholder="Password"
+                               value="<?= !empty($_POST['password']) ? htmlspecialchars($_POST['password'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') : ''; ?>"
+                        >
+
+                        <?php if (!empty($error['password'])): ?>
+                            <span class="form-text <?= !empty($error['password']) ? 'invalid-feedback' : '' ?>"><?= $error['password'] ?></span>
+                        <?php endif; ?>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="hidden" name="update">
+                        <?= !empty($_GET['id']) ? '<input type="hidden" name="id" value="' . htmlspecialchars($_GET['id'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') . '">' : ''; ?>
+                        <button type="submit" class="btn btn-primary btn-md float-right">Login</button>
+                    </div>
+                    </form>
+                </div>
             </div>
+
         </div>
     </div>
-
-    <div class="d-flex p-2 d-flex justify-content-center">
-        <div class="form-group row">
-            <label for="password" class="col-sm-2 col-md-6 col-form-label">Password <span style="color: #FF0000;">*</span></label>
-            <div class="col-sm-12 col-md-12">
-                <input id="password"
-                       type="password"
-                       name="password"
-                       class="form-control <?= !empty($error['password']) ? 'is-invalid' : '' ?>"
-                       placeholder="Password"
-                       value="<?= !empty($_POST['password']) ? htmlspecialchars($_POST['password'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') : '';?>">
-
-                <?php if (!empty($error['password'])): ?>
-                    <span id="passwordHelpBlock"
-                          class="form-text <?= !empty($error['password']) ? 'invalid-feedback' : '' ?>"><?= $error['password'] ?></span>
-                <?php endif; ?>
-
-            </div>
-        </div>
-    </div>
-
-    <div class="d-flex p-2 d-flex justify-content-center">
-        <div class="form-group row">
-            <div class="col-sm-12 col-md-12">
-                <button name="submit" type="submit" class="btn btn-primary">Sign In</button>&nbsp;
-                <a href="./forgot.php">Forgot Password</a>
-            </div>
-        </div>
-    </div>
-
-</form>
+</div>
