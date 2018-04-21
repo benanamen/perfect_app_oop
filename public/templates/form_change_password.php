3a3672079484c79673fcccd7bf0d9ac26d9da701
KR <ducatikevin@yahoo.com>
2018-04-21 14:37:13 -0700

    Code cleanup

--- public/templates/form_change_password.php
+++ public/templates/form_change_password.php
@@ -14,57 +14,56 @@ if (!defined('SECURE_PAGE'))
                     <h3 class="mb-0">Change Password</h3>
                 </div>
                 <div class="card-body">
-                    <form role="form" method="post" autocomplete="off"
-                    ">
+                    <form role="form" method="post" autocomplete="off">
 
-                    <div class="form-group">
-                        <label for="password">Current Password <span style="color: #FF0000;">*</span></label>
-                        <input id="password"
-                               name="password"
-                               type="password"
-                               class="form-control <?= !empty($error['password']) ? 'is-invalid' : '' ?>"
+                        <div class="form-group">
+                            <label for="password">Current Password <span style="color: #FF0000;">*</span></label>
+                            <input id="password"
+                                   name="password"
+                                   type="password"
+                                   class="form-control <?= !empty($error['password']) ? 'is-invalid' : '' ?>"
 
-                               value="<?= !empty($_POST['password']) ? htmlspecialchars($_POST['password'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') : ''; ?>"
-                        >
+                                   value="<?= !empty($_POST['password']) ? htmlspecialchars($_POST['password'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') : ''; ?>"
+                            >
 
-                        <?php if (!empty($error['password'])): ?>
-                            <span class="form-text <?= !empty($error['password']) ? 'invalid-feedback' : '' ?>"><?= $error['password'] ?></span>
-                        <?php endif; ?>
-                        <span id="helpPassword" class="form-text small text-muted">You will be automatically logged out after password change. You will need to re-login.</span>
-                    </div>
+                            <?php if (!empty($error['password'])): ?>
+                                <span class="form-text <?= !empty($error['password']) ? 'invalid-feedback' : '' ?>"><?= $error['password'] ?></span>
+                            <?php endif; ?>
+                            <span id="helpPassword" class="form-text small text-muted">You will be automatically logged out after password change. You will need to re-login.</span>
+                        </div>
 
-                    <div class="form-group">
-                        <label for="new_password">New Password <span style="color: #FF0000;">*</span></label>
-                        <input id="new_password"
-                               name="new_password"
-                               type="password"
-                               class="form-control <?= !empty($error['new_password']) ? 'is-invalid' : '' ?>"
-                               value="<?= !empty($_POST['new_password']) ? htmlspecialchars($_POST['new_password'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') : ''; ?>"
-                        >
+                        <div class="form-group">
+                            <label for="new_password">New Password <span style="color: #FF0000;">*</span></label>
+                            <input id="new_password"
+                                   name="new_password"
+                                   type="password"
+                                   class="form-control <?= !empty($error['new_password']) ? 'is-invalid' : '' ?>"
+                                   value="<?= !empty($_POST['new_password']) ? htmlspecialchars($_POST['new_password'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') : ''; ?>"
+                            >
 
-                        <?php if (!empty($error['new_password'])): ?>
-                            <span class="form-text <?= !empty($error['new_password']) ? 'invalid-feedback' : '' ?>"><?= $error['new_password'] ?></span>
-                        <?php endif; ?>
-                    </div>
+                            <?php if (!empty($error['new_password'])): ?>
+                                <span class="form-text <?= !empty($error['new_password']) ? 'invalid-feedback' : '' ?>"><?= $error['new_password'] ?></span>
+                            <?php endif; ?>
+                        </div>
 
-                    <div class="form-group">
-                        <label for="confirm_new_password">Confirm New Password <span
-                                    style="color: #FF0000;">*</span></label>
-                        <input id="confirm_new_password"
-                               name="confirm_new_password"
-                               type="password"
-                               class="form-control <?= !empty($error['confirm_new_password']) ? 'is-invalid' : '' ?>"
-                               value="<?= !empty($_POST['confirm_new_password']) ? htmlspecialchars($_POST['confirm_new_password'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') : ''; ?>"
-                        >
+                        <div class="form-group">
+                            <label for="password_confirm">Confirm Password <span
+                                        style="color: #FF0000;">*</span></label>
+                            <input id="password_confirm"
+                                   name="password_confirm"
+                                   type="password"
+                                   class="form-control <?= !empty($error['password_confirm']) ? 'is-invalid' : '' ?>"
+                                   value="<?= !empty($_POST['password_confirm']) ? htmlspecialchars($_POST['password_confirm'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') : ''; ?>"
+                            >
 
-                        <?php if (!empty($error['confirm_new_password'])): ?>
-                            <span class="form-text <?= !empty($error['confirm_new_password']) ? 'invalid-feedback' : '' ?>"><?= $error['confirm_new_password'] ?></span>
-                        <?php endif; ?>
-                    </div>
+                            <?php if (!empty($error['password_confirm'])): ?>
+                                <span class="form-text <?= !empty($error['password_confirm']) ? 'invalid-feedback' : '' ?>"><?= $error['password_confirm'] ?></span>
+                            <?php endif; ?>
+                        </div>
 
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-primary btn-md">Save</button>
-                    </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary btn-md">Save</button>
+                        </div>
                     </form>
                 </div>
             </div>
