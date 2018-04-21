b41b0f33732bbc810d7028486f0868be21ec4f18
KR <ducatikevin@yahoo.com>
2018-04-21 10:38:18 -0700

    Updated Change Password Form

--- public/includes/change_password.php
+++ public/includes/change_password.php
@@ -1,34 +1,36 @@
-<div class="col-md-6 offset-md-3">
-    <span class="anchor" id="formChangePassword"></span>
-    <hr class="mb-5">
+<div class="row">
+    <div class="col-md-12">
+        <div class="col-md-6 offset-md-3">
 
-    <!-- form card change password -->
-    <div class="card card-outline-secondary">
-        <div class="card-header bg-primary text-white">
-            <h3 class="mb-0">Change Password</h3>
-        </div>
-        <div class="card-body">
-            <form class="form" role="form" autocomplete="off">
-                <div class="form-group">
-                    <label for="inputPasswordOld">Current Password</label>
-                    <input type="password" class="form-control" id="inputPasswordOld" required="">
+            <!-- form card change password -->
+            <div class="card card-outline-secondary mt-5">
+                <div class="card-header bg-primary text-white">
+                    <h3 class="mb-0">Change Password</h3>
                 </div>
-                <div class="form-group">
-                    <label for="inputPasswordNew">New Password</label>
-                    <input type="password" class="form-control" id="inputPasswordNew" required="">
-                    <span class="form-text small text-muted">The password must be 8-20 characters, and must <em>not</em> contain spaces.</span>
+                <div class="card-body">
+                    <form class="form" role="form" autocomplete="off">
+                        <div class="form-group">
+                            <label for="inputPasswordOld">Current Password</label>
+                            <input type="password" class="form-control" id="inputPasswordOld" required="">
+                        </div>
+                        <div class="form-group">
+                            <label for="inputPasswordNew">New Password</label>
+                            <input type="password" class="form-control" id="inputPasswordNew" required="">
+                            <span class="form-text small text-muted">The password must be 8-20 characters, and must <em>not</em> contain spaces.</span>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputPasswordNewVerify">Verify New Password</label>
+                            <input type="password" class="form-control" id="inputPasswordNewVerify" required="">
+                            <span class="form-text small text-muted">To confirm, type the new password again.</span>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary btn-md float-right">Save</button>
+                        </div>
+                    </form>
                 </div>
-                <div class="form-group">
-                    <label for="inputPasswordNewVerify">Verify</label>
-                    <input type="password" class="form-control" id="inputPasswordNewVerify" required="">
-                    <span class="form-text small text-muted">To confirm, type the new password again.</span>
-                </div>
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary btn-md float-right">Save</button>
-                </div>
-            </form>
+            </div>
+            <!-- /form card change password -->
+
         </div>
     </div>
-    <!-- /form card change password -->
-
 </div>
