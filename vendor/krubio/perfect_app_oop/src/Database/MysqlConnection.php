8ce64044dae3c307b9c716d9b030d0a9b85144cc
KR <ducatikevin@yahoo.com>
2019-02-02 18:42:19 -0800

    Removed unnecessary Connection Try/Catch. Sending error email handled by set_exception_handler

--- vendor/krubio/perfect_app_oop/src/Database/MysqlConnection.php
+++ vendor/krubio/perfect_app_oop/src/Database/MysqlConnection.php
@@ -15,8 +15,6 @@ class MysqlConnection implements DatabaseConnection
      */
     public function connect()
     {
-        try
-        {
             $dsn = DB_TYPE . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET;
             $opt = [
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
@@ -24,16 +22,5 @@ class MysqlConnection implements DatabaseConnection
                   , PDO::ATTR_EMULATE_PREPARES => false];
 
             return new PDO($dsn, DB_USER, DB_PASSWORD, $opt);
-        }
-        catch (\PDOException $e)
-        {
-            $error = $e->getMessage() . ' in ' . $e->getFile() . ' on line ' . $e->getLine();
-            error_log(MYSQL_DATETIME_TODAY . "|$error\r\n", 3, ERROR_LOG_PATH);
-
-            $subject = "Database Down";
-            $email_body = "The Database is down for " . APP_NAME . "\n ERROR: $error";
-            send_email(ADMIN_EMAIL_TO, $subject, $email_body, ADMIN_EMAIL_FROM);
-            die('<h1><span style="color:red">FATAL ERROR: No Database Connection</span></h1>');
-        }
     }
 }
