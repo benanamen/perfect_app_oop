83d17191f25288f5ede58bc5c54aaefbc7fb77d2
benanamen <ducatikevin@yahoo.com>
2019-02-05 15:33:24 -0800

    Added path variable to SQLite Database

--- vendor/krubio/perfect_app_oop/src/Database/SQLiteConnection.php
+++ vendor/krubio/perfect_app_oop/src/Database/SQLiteConnection.php
@@ -16,11 +16,12 @@ class SQLiteConnection implements DatabaseConnection
      */
     public function connect()
     {
-        $opt = [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        $options = [
+              PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
             , PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
             , PDO::ATTR_EMULATE_PREPARES => false];
 
-        return new PDO('sqlite:C:\laragon\www\perfectappoop\vendor\krubio\perfect_app_oop\src\Database\dogsDb_PDO.db', null, null, $opt);
+        $path = SQLITE_DB_PATH;
+        return new PDO("sqlite:$path", null, null, $options);
     }
 }
